f952b1518da0c4dc392abfd2ceb11cc75aa85904
readyiolab <github@arbilo.com>
2025-07-30 01:01:07 +0530

    change in vite

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,12 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  server: {
-    host: '0.0.0.0', // 👈 expose to LAN
-    port: 5173,       // optional: specify port
   },
 })
